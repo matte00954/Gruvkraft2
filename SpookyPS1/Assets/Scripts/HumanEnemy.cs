304f9c290edc50a413866ac7225eb005450096fc
matte00954 <matte00954@gmail.com>
2020-06-30 17:40:33 +0200

    Liten uppdatering till AI scenen

--- SpookyPS1/Assets/Scripts/HumanEnemy.cs
+++ SpookyPS1/Assets/Scripts/HumanEnemy.cs
@@ -6,21 +6,28 @@ using UnityEngine.AI;
 public class HumanEnemy : MonoBehaviour
 {
     [Header("Enemy radius/ranges")]
-    public float lookRange = 50f; //hur långt fienden kan se
-    public float hearingRadius = 30f; //hur långt fienden kan "höra"
-    public float rotationSpeedMultiplier = 20f;
+    public float lookRange; //hur långt fienden kan se
+    public float hearingRadius; //hur långt fienden kan "höra"
+    public float rotationSpeedMultiplier;
 
-    private bool hearsPlayer;
+    //private bool hearsPlayer;
+    private bool seesPlayer;
 
     [Header("Patrol")]
     public List<Vector3> patrolRoutes; //denna verkar skapas utan "new"
+    private int currentPatrolPos;
 
     NavMeshAgent agent;
     Transform target;
+    Rigidbody rb;
+
+    // Bit shift the index of the layer (8) to get a bit mask
+    private int layerMask = 1 << 8;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
@@ -29,6 +36,10 @@ public class HumanEnemy : MonoBehaviour
     void Update()
     {
 
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        // https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+        layerMask = ~layerMask; 
         float distance = Vector3.Distance(target.position, transform.position); //distansen mellan spelare och fiende
         Looking(distance);
         //PatrolRoute();
@@ -37,6 +48,18 @@ public class HumanEnemy : MonoBehaviour
         {
             SimulateHearing();
         }
+
+        if(!seesPlayer)
+        {
+            PatrolRoute();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log(agent.transform.position);
+            Debug.Log(agent.destination);
+        }
+
     }
 
     private void FaceTarget()
@@ -57,8 +80,10 @@ public class HumanEnemy : MonoBehaviour
     private void Looking(float distance)
     {
         RaycastHit eyes;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out eyes, lookRange))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out eyes, lookRange, layerMask))
         {
+            seesPlayer = true;
+
             Debug.Log("Player Spotted!");
 
             agent.SetDestination(target.position);
@@ -68,14 +93,16 @@ public class HumanEnemy : MonoBehaviour
                 Attack();
                 FaceTarget();
             }
-
+        }
+        else
+        {
+            seesPlayer = false;
         }
     }
 
     private void SimulateHearing()
     {
         Debug.Log("Enemy heard player!");
-        hearsPlayer = true;
         FaceTarget();
     }
 
@@ -93,13 +120,33 @@ public class HumanEnemy : MonoBehaviour
 
     private void PatrolRoute()
     {
-        int i = 0;
-        agent.SetDestination(patrolRoutes[i]);
-        Debug.Log(patrolRoutes[i]);
-        if (agent.transform.position == patrolRoutes[i])
-        {
+        //Debug.Log(patrolRoutes[currentPatrolPos]);
+        //Debug.Log(agent.transform.position);
 
+        if (agent.transform.position.z == patrolRoutes[currentPatrolPos].z &&
+            agent.transform.position.x == patrolRoutes[currentPatrolPos].x)
+        {
+            Debug.Log("Agent reached patrol position");
+            if (currentPatrolPos >= patrolRoutes.Count)
+            {
+                currentPatrolPos = 0;
+            }
+
+            else
+            {
+                currentPatrolPos++;
+            }
+        }
+
+        else
+        {
+            Debug.Log("Patrol position assigned");
+            agent.SetDestination(patrolRoutes[currentPatrolPos]);
         }
     }
-
+    /* Problem
+     * Fienden kan inte gå till specifika patrull värden eftersom att fienden Y värde blir något specifkt långt tal, man måste skriva in exakt y värde
+     * Ta bort y värden och bara kolla på x och z värden? eller tänk om hela patrull lösningen?
+     * 
+    */
 }
