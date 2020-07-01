422f99701d25b05c55d673373185af552c1b0f3f
Joche007 <64202407+Joche007@users.noreply.github.com>
2020-07-01 21:05:37 +0200

    f2ef2ef
    2f

---
+++ SpookyPS1/Assets/Scripts/Trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject ob;
+    public string script;
+    public string method;
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag== "Player")
+        {
+            ob.SendMessage(method);
+        }
+    }
+}
