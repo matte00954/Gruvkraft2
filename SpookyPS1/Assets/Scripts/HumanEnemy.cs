080cbc3a9cfde8d4fda7c8647940867ea4126d6f
matte00954 <matte00954@gmail.com>
2020-07-06 13:30:53 +0200

    AI uppdatering

--- SpookyPS1/Assets/Scripts/HumanEnemy.cs
+++ SpookyPS1/Assets/Scripts/HumanEnemy.cs
@@ -12,10 +12,13 @@ public class HumanEnemy : MonoBehaviour
 
     //private bool hearsPlayer;
     private bool seesPlayer;
+    private bool patrolAssigned;
 
     [Header("Patrol")]
-    public List<Vector3> patrolRoutes; //denna verkar skapas utan "new"
+    public List<Transform> patrolRoutes; //denna verkar skapas utan "new"
+    public GameObject patrolPrefab;
     private int currentPatrolPos;
+    private bool reachedPatrolPos;
 
     NavMeshAgent agent;
     Transform target;
@@ -30,6 +33,12 @@ public class HumanEnemy : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+
+        for (int i = 0; i < patrolRoutes.Count; i++)
+        {
+            Instantiate(patrolPrefab, patrolRoutes[i]); //skapar spelobjekt för checkpoints
+        }
+
     }
 
     // Update is called once per frame
@@ -39,19 +48,13 @@ public class HumanEnemy : MonoBehaviour
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
         // https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
-        layerMask = ~layerMask; 
+        layerMask = ~layerMask;
         float distance = Vector3.Distance(target.position, transform.position); //distansen mellan spelare och fiende
         Looking(distance);
-        //PatrolRoute();
 
         if (distance <= hearingRadius) //om spelaren är i fiendens radius
         {
             SimulateHearing();
-        }
-
-        if(!seesPlayer)
-        {
-            PatrolRoute();
         }
 
         if (Input.GetKeyDown(KeyCode.I))
@@ -60,6 +63,21 @@ public class HumanEnemy : MonoBehaviour
             Debug.Log(agent.destination);
         }
 
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("patrolCheckpoint"))
+        {
+
+        }
+    }
+
+
+    private void LateUpdate()
+    {
+        PatrolRoute(); //tveksamt om jag vill ha detta i en update metod
     }
 
     private void FaceTarget()
@@ -82,21 +100,56 @@ public class HumanEnemy : MonoBehaviour
         RaycastHit eyes;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out eyes, lookRange, layerMask))
         {
-            seesPlayer = true;
 
-            Debug.Log("Player Spotted!");
+            if (eyes.collider.tag.Equals("Player"))
+            {
+                seesPlayer = true;
 
-            agent.SetDestination(target.position);
+                Debug.Log("Player Spotted!");
+
+                agent.SetDestination(target.position);
+
+            }
 
             if (distance <= agent.stoppingDistance)
             {
                 Attack();
                 FaceTarget();
             }
+            else
+            {
+                seesPlayer = false;
+            }
+        }
+    }
+
+    private void PatrolRoute()
+    {
+        if (reachedPatrolPos) //agent reach patrol pos
+        {
+
+            Debug.Log("Agent reached patrol position");
+            if (currentPatrolPos >= patrolRoutes.Count)
+            {
+                currentPatrolPos = 0;
+            }
+
+            else
+            {
+                currentPatrolPos++;
+            }
+
+            patrolAssigned = false;
+
         }
         else
         {
-            seesPlayer = false;
+            if (!patrolAssigned)
+            {
+                patrolAssigned = true;
+                Debug.Log("Patrol position assigned");
+                agent.SetDestination(patrolRoutes[currentPatrolPos].gameObject.transform.position);
+            }
         }
     }
 
@@ -105,7 +158,6 @@ public class HumanEnemy : MonoBehaviour
         Debug.Log("Enemy heard player!");
         FaceTarget();
     }
-
 
     private void Chasing() //min ide är att denna är igång i update OM fienden kan se spelaren det kommer behövas nån slags stealth state för spelaren
     {
@@ -118,35 +170,10 @@ public class HumanEnemy : MonoBehaviour
     }
 
 
-    private void PatrolRoute()
-    {
-        //Debug.Log(patrolRoutes[currentPatrolPos]);
-        //Debug.Log(agent.transform.position);
-
-        if (agent.transform.position.z == patrolRoutes[currentPatrolPos].z &&
-            agent.transform.position.x == patrolRoutes[currentPatrolPos].x)
-        {
-            Debug.Log("Agent reached patrol position");
-            if (currentPatrolPos >= patrolRoutes.Count)
-            {
-                currentPatrolPos = 0;
-            }
-
-            else
-            {
-                currentPatrolPos++;
-            }
-        }
-
-        else
-        {
-            Debug.Log("Patrol position assigned");
-            agent.SetDestination(patrolRoutes[currentPatrolPos]);
-        }
-    }
     /* Problem
      * Fienden kan inte gå till specifika patrull värden eftersom att fienden Y värde blir något specifkt långt tal, man måste skriva in exakt y värde
      * Ta bort y värden och bara kolla på x och z värden? eller tänk om hela patrull lösningen?
-     * 
+     * Om spelaren hörs, så bör det skapas en zon likt hearing zonen som man måste gå bort från för att fly från fienden
+     * Man skulle kunna randomisa positions värden nära spelaren för att få fienden att leta, om man blir hittad av en fiende. Kanske om man förbestämmer flera positioner innan och sedan så väljer fienden de som är närmast?
     */
 }
